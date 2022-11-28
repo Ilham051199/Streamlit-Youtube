22a48a6a442ed4ac62575c1109dc73d8ed35094d
Ilham051199 <119139282+Ilham051199@users.noreply.github.com>
2022-11-28 20:33:26 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import pickle
+from PIL import Image
+import streamlit as st
+import plotly
+plotly.offline.init_notebook_mode(connected=True)
+import plotly.offline as px
+st.title('Indonesia Youtube Trending Statictic')
+
+px.defaults.template = 'plotly_dark'
+px.defaults.color_continuous_scale = 'reds'
+
+
+
+img = Image.open('youtube.png')
+st.sidebar.image(img)
+
+# membuka file pickle
+with open("used_data.pickle", 'rb') as f:
+    data = pickle.load(f)
+
+#input tanggal
+min_date = data ['trending_date']. min()
+max_date = data ['trending_date']. max()
+start_date,end_date = st.sidebar.date_input(label='Rentang Waktu', min_value=min_date,max_value=max_date,value=[min_date,max_date])
+
+#input kategori
+categories = ["All categories"] + list(data['category'].value_counts().keys().sort_values())
+category= st.sidebar.selectbox(label='kategory',options=categories)
+
+#filter
+outputs = data[(data['trending_date'] >= start_date) & 
+                (data['trending_date'] <= end_date)]
+if category !="All Categories":
+    outputs = outputs[outputs['category']== category]
+
+# visualisaso dengan bar chart
+st.header(':video_camera: Channel')
+bar_data = outputs['channel_name'].value_counts().nlargest(10)
+fig=px.bar(bar_data, color=bar_data,orientation='h',title=f'Chanel Terpopuler Dari Kategori {category}')
+st.plotly_chart(fig)
+
+#visualisasi dengan scatter plot
+st.header(':bulb: Engagement')
+col1, col2 = st.columns(2)
+metrics_choice = ['like', 'dislike','comment']
+choice_1 = col1.selectbox('Horizontal', options=metrics_choice)
+choice_2 = col2.selectbox('Vertical', options=metrics_choice)
+fig = px.scatter(outputs,
+                x=choice_1,
+                y=choice_2,
+                size='view',
+                hover_name='channel_name',
+                hover_data=['title'],
+                title=f'Engagement of {choice_1.title()} and {choice_2.title()}')
+st.plotly_chart(fig)
